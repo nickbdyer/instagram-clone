d9644835abcdf3793f7b63a231b7e68e6ac1d338
Nick Dyer <nbdyer@gmail.com>
2014-12-20 16:37:09 +0000

    Shows posts on /posts

--- spec/features/posts_features_spec.rb
+++ spec/features/posts_features_spec.rb
@@ -9,5 +9,18 @@ feature 'Posts' do
       expect(page).to have_link("Add Post")
     end
   end
+
+  context 'when a post exists' do
+
+    before do
+      Post.create(content: "Hey check this out!")
+    end
+
+    scenario 'should show the posts' do
+      visit '/posts'
+      expect(page).to have_content("Hey check this out!")
+      expect(page).not_to have_content("There are no posts yet")
+    end
+  end
 end
 
