5f4cc06d1d26d8d02c7f3ad1a75722b9814f61c3
Nick Dyer <nbdyer@gmail.com>
2014-12-25 10:09:41 +0000

    Add missing image

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   has_attached_file :image,
     :styles => { :medium => "300x300>", :thumb => "100x100>"},
-    :default_url => "/images/:style/missing.png"
+    :default_url => ":style/missing.png"
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   has_many :comments
